0e268c1a9e5a311c2b2cbb256a93e24106d6075f
Anton Persson <antpe404@student.liu.se>
2016-10-13 19:04:56 +0200

    Wrote the tests for ridge

--- tests/testthat/tests_ridgereg.R
+++ tests/testthat/tests_ridgereg.R
@@ -1,18 +1,26 @@
 test_that("Ridge Test 1 ",{
   
-  mylm<-ridgereg(Petal.Width~Petal.Length,data=iris)
-  test<-lm.ridge(Petal.Width~Petal.Length,data=iris)
+  names(longley)[1] <- "y"
+  myridge<-ridgereg(formula=y ~ ., data=longley, lambda=2)
+  test<-lm.ridge(y~.,data=longley, lambda=2)
   
-  expect_equal(round(as.vector(mylm$coefficients), 10), round(as.vector(test$coefficients), 10))
-  expect_equal(round(as.vector(mylm$y.hat), 10), round(as.vector(test$fitted.values), 10))
-  expect_equal(class(mylm),"ridgereg")
+  myridge2<-ridgereg(formula=y ~ ., data=longley, lambda=1)
+  test2<-lm.ridge(y~.,data=longley, lambda=1)
+ 
+  
+  expect_equal(round(as.vector(myridge$coefficients[2:length(myridge$coefficients)])), round(as.vector(test$coef)))
+  #expect_equal(round(as.vector(mylm$y.hat), 10), round(as.vector(test$fitted.values), 10))
+  expect_equal(class(myridge),"ridgereg")
+  
+  expect_equal(round(as.vector(myridge2$coefficients[2:length(myridge$coefficients)])), round(as.vector(test2$coef)))
+  #expect_equal(round(as.vector(mylm$y.hat), 10), round(as.vector(test$fitted.values), 10))
+  expect_equal(class(myridge2),"ridgereg")
 })
 
 test_that("Ridge Test 1", {
   
-  expect_error(ridgereg(Petal.Width-Petal.Length,data=iris))
-  expect_error(ridgereg(Petal.Width,Petal.Length,data=iris))
+  expect_error(ridgereg(formula=y ~ , data=longley, lambda=2))
+  expect_error(ridgereg(Petal.Width~Petal.Length,data=iris[1:4,]))
   expect_error(ridgereg(Petallangd~Petal.Length,data=iris))
-  expect_warning(ridgereg(Petal.Width~Petal.Width,data=iris))
-  expect_error(ridgereg(c(1:20)~c(21:40)), data=iris)
+  expect_error(ridgereg(c(1:20)~c(21:40),data=longley, lambda=2))
 })
